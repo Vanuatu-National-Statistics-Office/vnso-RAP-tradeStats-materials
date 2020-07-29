0eac18ac408fa94ba56239299e5866c761a579bb
JosephCrispell <crispelljoseph@gmail.com>
2020-07-29 13:12:53 +0100

    Wrote code to extract sub tables from formatted Balance Of Trade table

--- R/functions.R
+++ R/functions.R
@@ -6,8 +6,7 @@
 #' @param position A character string determining how to position the table on a page. Possible values include \code{left}, \code{center}, \code{right}, \code{float_left} and \code{float_right}. 
 #' @param bootstrap_options A character vector for bootstrap table options. Possible options include \code{basic}, \code{striped}, \code{bordered}, \code{hover}, \code{condensed} and \code{responsive}.
 #' @param ... Arguments to be passed to \code{kableExtra::kable_styling} function 
-#' @keywords colour alpha transparency
-#' @export
+#' @keywords kable markdown kableExtra
 #' @examples
 #' # Define a data frame
 #' data <- data.frame("X"=rnorm(100), "Y"=1:100)
@@ -27,4 +26,31 @@ prettyTable <- function(table, full_width=FALSE, position="left", bootstrap_opti
     
     # Make the table scrollable
     scroll_box(height = "400px")
+}
+
+#' Extracting the Balance of Trade historic sub-tables from excel workbook with final formatted tbales
+#' 
+#' A function to extract the pre-inserted historic data in a currently formatted Balance of Trade Table
+#' @param fileName A character string of full path for formatted trade statistics tables in excel workbook
+#' @keywords BoT openxlsx
+extractHistoricBalanceOfTradeStatistics <- function(fileName){
+  
+  # Extract the full historic table
+  historic <- openxlsx::read.xlsx(fileName, sheet="1_BOT", skipEmptyRows=TRUE, startRow=5)
+  
+  # Identify when each sub-table starts
+  annualStart <- 1
+  monthlyStart <- which(historic[, 1] == "Monthly")
+  end <- which(historic[, 1] == "Notes:") - 1
+  
+  # Extract the Annual statistics
+  annually <- historic[1:(monthlyStart-1), ]
+  colnames(annually) <- c("Year", "blank", "Exports", "Re-exports", "Total", "Imports-CIF", "Balance")
+  
+  # Extract the monthly statistics
+  monthly <- historic[(monthlyStart+1):end, ]
+  colnames(monthly) <- c("Year", "Month", "Exports", "Re-exports", "Total", "Imports-CIF", "Balance")
+  
+  # Return the extract sub tables
+  return(list("Annually"=annually, "Monthly"=monthly))
 }
