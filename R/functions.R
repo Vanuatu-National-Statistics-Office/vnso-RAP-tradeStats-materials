d79402fb4392b9226e234642ccc5307864984f51
JosephCrispell <crispelljoseph@gmail.com>
2020-07-29 14:28:00 +0100

    Added functions to informatively update the formatted Balance of Trade table

--- R/functions.R
+++ R/functions.R
@@ -5,7 +5,7 @@
 #' @param full_width A TRUE or FALSE variable controlling whether the HTML table should have 100% width. Defaults to \code{FALSE}.
 #' @param position A character string determining how to position the table on a page. Possible values include \code{left}, \code{center}, \code{right}, \code{float_left} and \code{float_right}. 
 #' @param bootstrap_options A character vector for bootstrap table options. Possible options include \code{basic}, \code{striped}, \code{bordered}, \code{hover}, \code{condensed} and \code{responsive}.
-#' @param ... Arguments to be passed to \code{kableExtra::kable_styling} function 
+#' @param ... Arguments to be passed to \code{kableExtra::kable_styling} function
 #' @keywords kable markdown kableExtra
 #' @examples
 #' # Define a data frame
@@ -32,6 +32,7 @@ prettyTable <- function(table, full_width=FALSE, position="left", bootstrap_opti
 #' 
 #' A function to extract the pre-inserted historic data in a currently formatted Balance of Trade Table
 #' @param fileName A character string of full path for formatted trade statistics tables in excel workbook
+#' @return Returns list with dataframes representing the Annual and Monthly sub tables and notes
 #' @keywords BoT openxlsx
 extractHistoricBalanceOfTradeStatistics <- function(fileName){
   
@@ -45,12 +46,86 @@ extractHistoricBalanceOfTradeStatistics <- function(fileName){
   
   # Extract the Annual statistics
   annually <- historic[1:(monthlyStart-1), ]
-  colnames(annually) <- c("Year", "blank", "Exports", "Re-exports", "Total", "Imports-CIF", "Balance")
+  colnames(annually) <- c("Year", "blank", "Export", "Re-Export", "Total Export", "Imports CIF", "Trade Balance")
   
   # Extract the monthly statistics
   monthly <- historic[(monthlyStart+1):end, ]
-  colnames(monthly) <- c("Year", "Month", "Exports", "Re-exports", "Total", "Imports-CIF", "Balance")
+  colnames(monthly) <- c("Year", "Month", "Export", "Re-Export", "Total Export", "Imports CIF", "Trade Balance")
+  
+  # Extract the notes from the table
+  notes <- historic[(end+1):nrow(historic), c(1,2)]
+  colnames(notes) <- c("Notes", "blank")
   
   # Return the extract sub tables
-  return(list("Annually"=annually, "Monthly"=monthly))
+  return(list("Annually"=annually, "Monthly"=monthly, "Notes"=notes))
+}
+
+#' Update Annual and Monthly sub-tables of the Balance of Trade table
+#' 
+#' A function that informatively updates the annual and monthly tables. In December add row to annual table. In January include year in monthly table.
+#' @param subTables A list with dataframes representing the Annual and Monthly sub tables
+#' @param month Full name of month with first letter upper case
+#' @param year Full year
+#' @param newTradeBalanceStatistics Data.frame with single row storing balance of trade statistics for current month
+#' @return Returns list with updated dataframes representing the Annual and Monthly sub tables and notes
+#' @keywords BoT openxlsx
+updateBalanceOfTradeSubTables <- function(subTables, month, year, newTradeBalanceStatistics){
+  
+  # Check if December
+  december <- month == "December"
+  
+  # Check if we need to update the annual table
+  if(month == "December"){
+    
+    # Calculate the sum of statistics for the last year
+    januaryRows <- which(subTables$Monthly == "January")
+    lastYearsRows <- januaryRows[length(januaryRows)]:nrow(subTables$Monthly)
+    currentYearsTotals <- data.frame("Year"=year, "blank"=NA,
+                                     "Export"=sum(subTables$Monthly$Export[lastYearsRows]) + newTradeBalanceStatistics$Export,
+                                     "Re-Export"=sum(subTables$Monthly$`Re-Export`[lastYearsRows]) + newTradeBalanceStatistics$`Re-Export`,
+                                     "Total Export"=sum(subTables$Monthly$`Total Export`[lastYearsRows]) + newTradeBalanceStatistics$`Total Export`,
+                                     "Imports CIF"=sum(subTables$Monthly$`Imports CIF`[lastYearsRows]) + newTradeBalanceStatistics$`Imports CIF`,
+                                     "Trade Balance"=sum(subTables$Monthly$`Trade Balance`[lastYearsRows]) + newTradeBalanceStatistics$`Trade Balance`,
+                                     stringsAsFactors=FALSE)
+    
+    # Add a new row to the annual table for this years data
+    subTables$Annually <- rbind(subTables$Annually, currentYearsTotals)
+  }
+  
+  # Update the monthly table
+  newTradeBalanceStatistics$Year <- ifelse(month == "January", year, NA)
+  newTradeBalanceStatistics$Month <- month
+  newTradeBalanceStatistics <- rbind(ifelse(month == "January", NA, NULL), newTradeBalanceStatistics)
+  subTables$Monthly <- rbind(subTables$Monthly, newTradeBalanceStatistics)
+  
+  return(subTables)
+}
+
+#' Inserts updated Balance of Trade sub tables back into formatted excel sheet
+#' 
+#' A function that inserts the updated Annual and Monthly statistics back into formatted Balance of Trade table 
+#' @param fileName A character string of full path for formatted trade statistics tables in excel workbook
+#' @param subTables A list with dataframes representing the Annual and Monthly sub tables
+#' @param nRowsInHeader The number of rows that make up the header of the formatted Balance of Trade table
+#' @keywords BoT openxlsx
+updatedFinalFormattedBalanceOfTradeTable <- function(fileName, subTables, nRowsInHeader=6){
+
+  # Load the final tables workbook for editing
+  finalWorkbook <- loadWorkbook(fileName)
+  
+  # Insert the Annual sub table
+  writeData(finalWorkbook, sheet="1_BOT", startCol=1, startRow=nRowsInHeader, x=subTables$Annually, colNames=FALSE)
+  nRowsInAnnual <- nrow(subTables$Annually)
+  
+  # Insert the monthly sub table
+  writeData(finalWorkbook, sheet="1_BOT", startCol=1, startRow=nRowsInHeader+nRowsInAnnual+1, x="Monthly", colNames=FALSE)
+  writeData(finalWorkbook, sheet="1_BOT", startCol=1, startRow=nRowsInHeader+nRowsInAnnual+2, x=subTables$Monthly, colNames=FALSE)
+  nRowsInMonthly <- nrow(subTables$Monthly)
+  
+  # Insert the notes
+  writeData(finalWorkbook, sheet="1_BOT", startCol=1, startRow=nRowsInHeader+nRowsInAnnual+2+nRowsInMonthly, x=rbind(NA, subTables$Notes), colNames=FALSE)
+  
+  # Save the edited workbook as a new file
+  editedFinalWorkbookFileName <- gsub(pattern=".xlsx", replacement="_EDITED.xlsx", fileName)
+  saveWorkbook(finalWorkbook, file=editedFinalWorkbookFileName, overwrite=TRUE)
 }
