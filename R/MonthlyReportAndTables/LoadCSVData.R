230f10e23401adb071849ed24d14073aac6b93f9
JosephCrispell <crispelljoseph@gmail.com>
2021-04-28 10:31:41 +0100

    Working on setting column classes

--- R/MonthlyReportAndTables/LoadCSVData.R
+++ R/MonthlyReportAndTables/LoadCSVData.R
@@ -17,10 +17,41 @@ secureDataFolder <- file.path(repository, "data", "secure")
 # Read in the raw trade data from secure folder of the repository 
 # Note that spaces in column names have been automatically replaced with "."s
 tradeStatsFile <- file.path(secureDataFolder, "SEC_PROC_ASY_RawData_31-01-20.csv")
-tradeStats <- read.csv(tradeStatsFile)
+tradeStats <- read.csv(tradeStatsFile, check.names = FALSE)
 
 # Remove the repeated header row from the trade statistics data
 tradeStats <- tradeStats[tradeStats$Office != "Office", ]
+
+# Set the column classes
+colClasses = c("Office" = "factor",
+               "Reg.Ref" = "character",
+               "Reg. Date" = "character",
+               "Type" = "factor",
+               "CP4" = "factor",
+               "CP3" = "factor",
+               "Declarant" = "numeric",
+               "Itm #" = "numeric",
+               "HS Code" = "factor",
+               "SITC" = "factor",
+               "PRF" = "character",
+               "Goods Comm. Dsc 1" = "character",
+               "Goods Comm. Dsc." = "character",
+               "CO" = "factor",
+               "CE/CD" = "character",
+               "Wgt. Gross" = "numeric",
+               "Wgt. Net" = "numeric",
+               "Pkg #" = "numeric",
+               "Pkg  type" = "character",
+               "Supp Qty" = "numeric",
+               "TOD" = "factor",
+               "Stat. Value" = "character",
+               "IMD" = "character",
+               "VAT" = "character",
+               "IEX" = "numeric",
+               "DEX" = "numeric",
+               "EXD" = "numeric",
+               "OED" = "numeric")
+
 
 # Note the date column as dates
 tradeStats$`Reg..Date` <- as.Date(tradeStats$`Reg..Date`, format = "%d/%m/%Y")
