8839ce382a37623d116ec39ce8f949f8cfd91f3b
JosephCrispell <crispelljoseph@gmail.com>
2021-05-12 12:33:34 +0100

    Added raw data row ID when checking commodity values against expected ranges

--- R/MonthlyReportAndTables/1_processLatestMonthsTradeStats.R
+++ R/MonthlyReportAndTables/1_processLatestMonthsTradeStats.R
@@ -237,7 +237,7 @@ commoditiesWithExpectations <- checkCommodityValues(tradeStatsCommoditiesMergedW
                                                     historicImportsSummaryStats, historicExportsSummaryStats,
                                                     importCP4s=c(4000, 4071, 7100), exportCP4s=c(1000, 3071), useUnitValue=FALSE,
                                                     columnsOfInterest = c("HS.Code", "Type", "Reg..Date", 
-                                                                          "CP4", "Itm.."))
+                                                                          "CP4", "Itm..", "RawDataRowID"))
 
 # Print progress
 cat("Finished checking whether commodity values fall outside of expectations based on historic data.\n")
